c1f52482c7caf083d1417b6e08ffe803366a2539
Cytla24 <ojuba2013@gmail.com>
2020-04-05 23:37:24 -0400

    changed a few lines, testing lexical analyzer

--- ass.c
+++ ass.c
@@ -40,18 +40,19 @@ void error();
 
 /******************************************************/
 /* main driver */
-int main() {
-/* Open the input data file and process its contents */
- if ((in_fp = fopen("front.in", "r")) == NULL)
- printf("ERROR - cannot open front.in \n");
- else {
- getChar();
- do {
- lex();
- } while (nextToken != EOF);
- }
- return 0;
-}
+	int main() {
+	/* Open the input data file and process its contents */
+	in_fp = fopen("front.txt", "r");
+	if (in_fp == NULL)
+		printf("ERROR - cannot open front.in \n");
+		else {
+			getChar();
+			do {
+				lex();
+			} while (nextToken != EOF);
+		}
+		return 0;
+	}
 
 /*****************************************************/
 /* lookup - a function to lookup operators and parentheses
